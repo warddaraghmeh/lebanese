1ac5d0ebf05b914e1ea6105a2329b031e3740e6b
warddaraghmeh <warddaraghmeh99@gmail.com>
2023-09-19 17:33:05 +0300

    Form Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";  
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+     console.log(formData);
+   };
+
+  const isFormValid =
+    formData.firstName.trim() !== "" &&
+    formData.lastName.trim() !== "" &&
+    formData.email.trim() !== "";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="animated-form">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="1" className="form-label">
+          First name:
+        </label>
+        <input
+          type="text"
+          id="1"
+          name="firstName"
+          placeholder="First Name"
+          required
+          value={formData.firstName}
+          onChange={(e) =>
+            setFormData({ ...formData, firstName: e.target.value })
+          }
+          className="form-input"
+        />
+        <label htmlFor="2" className="form-label">
+          Last name:
+        </label>
+        <input
+          type="text"
+          id="2"
+          name="lastName"
+          placeholder="Last Name"
+          required
+          value={formData.lastName}
+          onChange={(e) =>
+            setFormData({ ...formData, lastName: e.target.value })
+          }
+          className="form-input"
+        />
+        <label htmlFor="3" className="form-label">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="3"
+          name="email"
+          placeholder="Email"
+          required
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          className="form-input"
+        />
+        <button
+          type="submit"
+          className={`submit-button glow-on-hover ${
+            isFormValid ? "" : "move-button"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
